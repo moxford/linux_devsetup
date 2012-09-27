89f21770117be08884d567fe43104a0f214d38a1
moxford <moxford@moxbuntu.corp.playdom.com>
2012-09-26 17:54:32 -0700

    more install

--- install.sh
+++ install.sh
@@ -1,3 +1,29 @@
 #!/bin/sh
-ln -s linux_devsetup/.emacs.d .emacs.d
-ln -s linux_devsetup/.emacs .emacs
+
+DEVSETUP="~/linux_devsetup"
+
+symlink_devsetup () {
+    if [ [ -f ~/$1 || [ -d ~/$1 ] ]; then
+	mv ~/$1 ~/$1_backup
+    else if [ -h ~/$1 ]; then
+	echo "ERROR: ~/$1 is already a symlink!"
+	exit 1
+    else
+	echo "ERROR: ~/$1 is not a file or directory!"
+	exit 1
+    fi
+
+    if [ ! -e $1 ]; then
+	ln -s $DEVSETUP/$1 ~/$1
+    fi
+}
+
+symlink_devsetup test
+exit 0
+
+symlink_devsetup .emacs
+symlink_devsetup .emacs.d
+
+echo "source $DEVSETUP/.bash_profile >> ~/.bash_profile"
+echo "source $DEVSETUP/.bash_rc >> ~/.bash_rc"
+echo "source $DEVSETUP/.bash_aliases >> ~/.bash_aliases"
